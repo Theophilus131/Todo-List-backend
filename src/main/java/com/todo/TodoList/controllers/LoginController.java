0587448e0487ac827372cfd4ae2521a354aafdc3
Theophilus danladi umar <theophilusumar27@gmail.com>
2025-09-16 12:17:09 +0100

    Add LoginController to handle user authentication requests

---
+++ src/main/java/com/todo/TodoList/controllers/LoginController.java
@@ -0,0 +1,28 @@
+package com.todo.TodoList.controllers;
+
+
+import com.todo.TodoList.dto.request.LoginRequestDto;
+import com.todo.TodoList.dto.response.UserResponseDto;
+import com.todo.TodoList.service.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class LoginController {
+
+    @Autowired
+    private UserServices userServices;
+
+    @PostMapping("/login")
+    public UserResponseDto login(@RequestBody LoginRequestDto loginRequestDto){
+        return userServices.login(loginRequestDto);
+    }
+
+
+
+
+}
