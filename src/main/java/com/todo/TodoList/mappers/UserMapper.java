b94763c035224fea4a453cfb924d0356a5fc0570
Theophilus danladi umar <theophilusumar27@gmail.com>
2025-09-16 14:10:50 +0100

    Add UserMapper for converting between User entity and DTOs

---
+++ src/main/java/com/todo/TodoList/mappers/UserMapper.java
@@ -0,0 +1,29 @@
+package com.todo.TodoList.mappers;
+
+import com.todo.TodoList.data.models.User;
+import com.todo.TodoList.dto.request.UserRequestDto;
+import com.todo.TodoList.dto.response.UserResponseDto;
+
+
+public class UserMapper {
+
+    public static User toEntity(UserRequestDto userRequest){
+        User user = new User();
+
+        user.setName(userRequest.getName());
+        user.setEmail(userRequest.getEmail());
+        user.setPassword(userRequest.getPassword());
+
+        return user;
+    }
+
+    public static UserResponseDto toDto(User user){
+        UserResponseDto userResponse = new UserResponseDto();
+
+        userResponse.setId(user.getId());
+        userResponse.setName(user.getName());
+        userResponse.setEmail(userResponse.getEmail());
+
+        return userResponse;
+    }
+}
