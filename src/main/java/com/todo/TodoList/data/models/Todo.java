0687c7ced09edf4433878709df968fdad4e0b9ea
Theophilus danladi umar <theophilusumar27@gmail.com>
2025-09-16 12:12:01 +0100

    Add Todo model with MongoDB integration

---
+++ src/main/java/com/todo/TodoList/data/models/Todo.java
@@ -0,0 +1,24 @@
+package com.todo.TodoList.data.models;
+
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Document(collection = "todoDataBase")
+public class Todo {
+
+    @Id
+
+    private String id;
+    private String title;
+    private String description;
+    private Boolean completed;
+    private LocalDateTime creationDate;
+    private LocalDateTime dueDate;
+
+}
+
