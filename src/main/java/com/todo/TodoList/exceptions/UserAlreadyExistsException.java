478e49ab55f85e3ec3eb232a7bda4658d2d7604a
Theophilus danladi umar <theophilusumar27@gmail.com>
2025-09-16 12:14:56 +0100

    Add InvalidCredentialsException for handling authentication errors

---
+++ src/main/java/com/todo/TodoList/exceptions/UserAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.todo.TodoList.exceptions;
+
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
