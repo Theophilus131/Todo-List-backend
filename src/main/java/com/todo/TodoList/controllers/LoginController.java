4b1976faacd6188ef7c417fa115ce91f650d0167
Theophilus danladi umar <theophilusumar27@gmail.com>
2025-09-16 17:51:48 +0100

    Add Maven Wrapper script and enable validation for login requests

--- src/main/java/com/todo/TodoList/controllers/LoginController.java
+++ src/main/java/com/todo/TodoList/controllers/LoginController.java
@@ -4,6 +4,7 @@ package com.todo.TodoList.controllers;
 import com.todo.TodoList.dto.request.LoginRequestDto;
 import com.todo.TodoList.dto.response.UserResponseDto;
 import com.todo.TodoList.service.UserServices;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +19,7 @@ public class LoginController {
     private UserServices userServices;
 
     @PostMapping("/login")
-    public UserResponseDto login(@RequestBody LoginRequestDto loginRequestDto){
+    public UserResponseDto login(@Valid @RequestBody LoginRequestDto loginRequestDto){
         return userServices.login(loginRequestDto);
     }
 
