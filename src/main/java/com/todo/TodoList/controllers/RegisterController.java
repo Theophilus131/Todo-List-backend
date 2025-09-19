5bd2c964e8e4c41e8023498135cd18586b38fd4a
Theophilus danladi umar <theophilusumar27@gmail.com>
2025-09-19 11:24:31 +0100

    Add Maven Wrapper script and enable validation for login requests

--- src/main/java/com/todo/TodoList/controllers/RegisterController.java
+++ src/main/java/com/todo/TodoList/controllers/RegisterController.java
@@ -6,11 +6,10 @@ import com.todo.TodoList.dto.response.UserResponseDto;
 import com.todo.TodoList.service.UserServices;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+
+@CrossOrigin(origins = "http://127.0.0.1:5501")
 @RestController
 @RequestMapping("/auth")
 public class RegisterController {
