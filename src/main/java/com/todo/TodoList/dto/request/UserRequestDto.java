bca72c2a909c9df734d6e227a994b54d7e7d3883
Theophilus danladi umar <theophilusumar27@gmail.com>
2025-09-21 16:28:26 +0100

    Enhance validation in UserRequestDto for email format, password strength, and length

--- src/main/java/com/todo/TodoList/dto/request/UserRequestDto.java
+++ src/main/java/com/todo/TodoList/dto/request/UserRequestDto.java
@@ -2,14 +2,23 @@ package com.todo.TodoList.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserRequestDto {
 
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
     private String name;
 
+
+    @Pattern(
+            regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$",
+            message = "Invalid email format"
+    )
     @NotBlank(message = "required email input")
     @Email(message = "invalid email format")
     private String email;
