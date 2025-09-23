8c5b6673db8baa5aab7e47014e176d932aeebc3a
Theophilus danladi umar <theophilusumar27@gmail.com>
2025-09-23 03:43:40 +0100

    Add `findByUserId` method to TodoRepository to fetch tasks by user

--- src/main/java/com/todo/TodoList/data/repository/TodoRepository.java
+++ src/main/java/com/todo/TodoList/data/repository/TodoRepository.java
@@ -4,9 +4,13 @@ import com.todo.TodoList.data.models.Todo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TodoRepository extends MongoRepository<Todo,String> {
-    Optional<Todo> findById(String Id);
+
+    List<Todo> findByUserId(String userId);
+
 }
