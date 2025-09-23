c04d6aa9f0ba2d14fda3ed9f0a31a792cb184d62
Theophilus danladi umar <theophilusumar27@gmail.com>
2025-09-23 03:43:04 +0100

    Rename `id` field to `userid` in User model for better clarity

--- src/main/java/com/todo/TodoList/data/models/User.java
+++ src/main/java/com/todo/TodoList/data/models/User.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class User {
 
     @Id
-    private String id;
+    private String userid;
     private String name;
     private String email;
     private String password;
