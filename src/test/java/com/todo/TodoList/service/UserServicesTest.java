f39e087dda6d0ee727194389d728225f5ded259d
Theophilus danladi umar <theophilusumar27@gmail.com>
2025-09-25 03:50:41 +0100

    Add test for `registerUser` in `UserServicesTest` to verify user registration behavior

--- src/test/java/com/todo/TodoList/service/UserServicesTest.java
+++ src/test/java/com/todo/TodoList/service/UserServicesTest.java
@@ -1,5 +1,6 @@
 package com.todo.TodoList.service;
 
+import com.todo.TodoList.data.models.User;
 import com.todo.TodoList.data.repository.UserRepository;
 import com.todo.TodoList.dto.request.UserRequestDto;
 import com.todo.TodoList.dto.response.UserResponseDto;
@@ -9,6 +10,10 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.OPTIONAL;
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -19,8 +24,14 @@ class UserServicesTest {
     @Autowired
     private UserServices userServices;
 
+    private UserRequestDto userRequestDto;
+
     @BeforeEach
     void setUp() {
+        userRequestDto = new UserRequestDto();
+        userRequestDto.setName("theo");
+        userRequestDto.setEmail("theo@12.com");
+        userRequestDto.setPassword("password123");
         userRepository.deleteAll();
 
 
@@ -28,10 +39,14 @@ class UserServicesTest {
 
     @Test
     void testRegisterNewUser(){
-        UserRequestDto userRequestDto = new UserRequestDto();
+       UserResponseDto saved = userServices.registerUser(userRequestDto);
 
-         userServices.registerUser(userRequestDto);
-         assertEquals(1,userRepository.count());
+       assertThat(saved.getName()).isEqualTo("theo");
+       assertThat(saved.getEmail()).isEqualTo("theo@12.com");
+
+
+       Optional<User> userInDatabase = userRepository.findByEmail("theo@12.com");
+       assertThat(userInDatabase).isPresent();
 
     }
 
